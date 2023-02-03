051514aeffa3e7f7386166b7820ad8b8e65769cd
sofonias101 <sofoniasasefaw@gmail.com>
2023-02-03 10:59:24 -0800

    Added description for and Find Intersection updated.

--- Find Intersection.php
+++ Find Intersection.php
@@ -1,15 +1,33 @@
 <?php
-$strArr = array("1, 3, 9, 10, 17, 18", "1, 4, 9, 10");
+
+/* Find Intersection
+
+Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements:
+the first element will represent a list of comma-separated numbers sorted in ascending order,
+the second element will represent a second list of comma-separated numbers (also sorted).
+Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order.
+If there is no intersection, return the string false.*/
+
+
+$strArr = array("1, 2, 3, 4, 5", "6, 7, 8, 9, 10");
 
 
 function FindIntersection($strArr)
 {
     // code goes here
-    return $diff = implode(",",
+    if (implode(",",
         array_intersect(explode(", ", $strArr[0]),
             explode(", ", $strArr[1])
         )
-    );
+    )) {
+       return implode(",",
+            array_intersect(explode(", ", $strArr[0]),
+                explode(", ", $strArr[1])
+            )
+        );
+} else {
+        echo "false";
+    }
 
 }
 
